dd1cecdbe24d31b60ca8fec240148a02a84bd6ec
Felipe <lfazocar@users.noreply.github.com>
2023-06-29 12:38:10 -0400

    feat: pages controller and template assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'index', to: "pages#index"
+  get 'about', to: "pages#about"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#index"
 end
